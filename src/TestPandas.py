d5619aff47b1f735366873a26da89cb55a05a3be
Palaciosgmr <Chevy802473>
2018-06-23 13:38:24 -0500

    Creacion de clase de test

--- src/TestPandas.py
+++ src/TestPandas.py
@@ -5,8 +5,19 @@ with open("v4test.csv", 'r') as csvfile:
     df = df.drop(['DESCRIPCION_CIC', 'CODIGO_VST','FECHA_PLANIFICADA_VST', 'FECHA_CREACION_USW'], axis=1)
 
      # Escenario 1 Numero de visitas anual por empleado
-    df_new1=df.groupby(['PRIMERAPELLIDO_US','SEGUNDOAPELLIDO_US','PRIMERNOMBRE_US','SEGUNDONOMBRE_US'])['NUMEROCONTACTO_VST'].sum()
+    df_new1=df.groupby(['PRIMERAPELLIDO_US','SEGUNDOAPELLIDO_US','PRIMERNOMBRE_US','SEGUNDONOMBRE_US'])['NUMEROCONTACTO_VST'].max()
     print(df_new1)
+
+
+    def add_numbers(x, y, z=None):
+        if (z == None):
+            return x + y
+        else:
+            return x + y + z
+
+
+    print(add_numbers(1, 2))
+    print(add_numbers(1, 2, 3))
     df_new1.to_csv('Escenario1.csv')
 
 
