aad435336686a6adb1e69ad04762562f4a3d8c8c
Palaciosgmr <Chevy802473>
2018-06-23 10:51:13 -0500

    Creacion de clase de test

--- src/TestPandas.py
+++ src/TestPandas.py
@@ -1,17 +1,24 @@
-import unittest
 import pandas as pd
 import numpy as np
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        s=pd.Series([1, 3, 5, np.nan, 6, 8])
-
-        df=pd.read_csv('../resources/v4test.csv',sep=';',encoding='utf-8',errors='ignore')
-        print(df)
 
 
-def add_numbers(x, y):
-    return x + y
+def main():
+    with open('../resources/v4test.csv') as csvfile:
 
-add_numbers(1, 2)
+        df = pd.read_csv(csvfile)
+        df = df.drop(['DESCRIPCION_CIC', 'CODIGO_VST','FECHA_PLANIFICADA_VST', 'FECHA_CREACION_USW'], axis=1)
 
+        # Escenario 1 Numero de visitas anual por empleado
+        df_new1=df.groupby(['PRIMERAPELLIDO_US','SEGUNDOAPELLIDO_US','PRIMERNOMBRE_US','SEGUNDONOMBRE_US'])['NUMEROCONTACTO_VST'].sum()
+        print(df_new1)
+        df_new1.to_csv('Escenario1.csv')
+
+        # Escenario 2 Empresa numero de visitas
+        df_new2 = df.groupby(['NOMBRE_EMP','FECHA_VST'])['NUMEROCONTACTO_VST'].sum()
+        df_new2.to_csv('Escenario2.csv')
+        print(df_new2)
+
+
+if __name__ == "__main__":
+    main()
