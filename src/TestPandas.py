5d4610b2ba41698d5b774ecebcc8602eec9817e7
Palaciosgmr <Chevy802473>
2018-06-23 10:41:02 -0500

    Pruebas

--- src/TestPandas.py
+++ src/TestPandas.py
@@ -1,12 +1,17 @@
 import unittest
+import pandas as pd
+import numpy as np
 
-'''
-Clase creada de prueba para pandas
-'''
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, True)
+        s=pd.Series([1, 3, 5, np.nan, 6, 8])
+
+        df=pd.read_csv('../resources/v4test.csv',sep=';',encoding='utf-8',errors='ignore')
+        print(df)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def add_numbers(x, y):
+    return x + y
+
+add_numbers(1, 2)
+
