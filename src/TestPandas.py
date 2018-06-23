dd5f8487f6dab6bf105debe979fb9f8919e028d6
Palaciosgmr <Chevy802473>
2018-06-23 14:06:54 -0500

    Escenarios

--- src/TestPandas.py
+++ src/TestPandas.py
@@ -1,24 +1,57 @@
 import pandas as pd
-with open("v4test.csv", 'r') as csvfile:
-    df = pd.read_csv(csvfile)
-    print(df)
-    df = df.drop(['DESCRIPCION_CIC', 'CODIGO_VST','FECHA_PLANIFICADA_VST', 'FECHA_CREACION_USW'], axis=1)
+import numpy as np
+class Calcular():
+    def escenario1(df,tarea):
+        tamano=len(df.index)
+        escenario=pd.DataFrame(index=df.index[tarea:tamano],columns=range(0,tarea+1))
+        for i in range(0,tamano-tarea):
+            x1=df.ix[df.index[i:i+tarea],'NOMBRE_ESV'].values
+            x1=np.append(x1,['0'])
+            x2=np.transpose(x1)
+            escenario.ix[escenario.index[i]]=x2
+        escenario.ix[escenario.index[0:tamano-tarea],tarea]=df.ix[df.index[tarea:tamano],'NOMBRE_ESV']
+        return escenario
 
-     # Escenario 1 Numero de visitas anual por empleado
-    df_new1=df.groupby(['PRIMERAPELLIDO_US','SEGUNDOAPELLIDO_US','PRIMERNOMBRE_US','SEGUNDONOMBRE_US'])['NUMEROCONTACTO_VST'].max()
-    print(df_new1)
+    def escenario2(df,tarea,empresa1):
+        tamano=len(df.index)
+        escenario=pd.DataFrame(index=df.index[tarea:tamano],columns=range(0,tarea+2))
+        for i in range(0,tamano-tarea):
+            x1=df.ix[df.index[i:i+tarea],empresa1].values
+            x1=np.append(x1,['0','0'])
+            x2=np.transpose(x1)
+            escenario.ix[escenario.index[i]]=x2
+        escenario.ix[escenario.index[0:tamano-tarea],tarea]=df.ix[df.index[tarea:tamano],empresa1]
+        escenario.ix[escenario.index[0:tamano-tarea],tarea+1]=df.ix[df.index[tarea:tamano],'NOMBRE_ESV']
+        return escenario
 
+    def escenario3(df,tarea,empresa1,empresa2):
+        tamano=len(df.index)
+        escenario=pd.DataFrame(index=df.index[tarea:tamano],columns=range(0,2*tarea+3))
+        for i in range(0,tamano-tarea):
+            x1=df.ix[df.index[i:i+tarea],empresa1].values
+            x11=df.ix[df.index[i:i+tarea],empresa2].values
+            xx=np.ravel(np.column_stack((x1,x11)))
+            xx=np.append(xx,['0','0','0'])
+            x2=np.transpose(xx)
+            escenario.ix[escenario.index[i]]=x2
+        escenario.ix[escenario.index[0:tamano-tarea],2*tarea]=df.ix[df.index[tarea:tamano],empresa1]
+        escenario.ix[escenario.index[0:tamano-tarea],2*tarea+1]=df.ix[df.index[tarea:tamano],empresa2]
+        escenario.ix[escenario.index[0:tamano-tarea],2*tarea+2]=df.ix[df.index[tarea:tamano],'NOMBRE_ESV']
+        return escenario
 
-    def add_numbers(x, y, z=None):
-        if (z == None):
-            return x + y
-        else:
-            return x + y + z
-
-
-    print(add_numbers(1, 2))
-    print(add_numbers(1, 2, 3))
-    df_new1.to_csv('Escenario1.csv')
-
-
-
+    def escenario4(df, tarea, empresa1, empresa2, empresa3):
+        tamano = len(df.index)
+        escenario = pd.DataFrame(index=df.index[tarea:tamano], columns=range(0, 3 * tarea + 4))
+        for i in range(0, tamano - tarea):
+            x1 = df.ix[df.index[i:i + tarea], empresa1].values
+            x11 = df.ix[df.index[i:i + tarea], empresa2].values
+            x111 = df.ix[df.index[i:i + tarea], empresa3].values
+            xx = np.ravel(np.column_stack((x1, x11,x111)))
+            xx = np.append(xx, ['0', '0', '0','0'])
+            x2 = np.transpose(xx)
+            escenario.ix[escenario.index[i]] = x2
+        escenario.ix[escenario.index[0:tamano - tarea], 3 * tarea] = df.ix[df.index[tarea:tamano], empresa1]
+        escenario.ix[escenario.index[0:tamano - tarea], 3 * tarea+1] = df.ix[df.index[tarea:tamano], empresa2]
+        escenario.ix[escenario.index[0:tamano - tarea], 3 * tarea+2] = df.ix[df.index[tarea:tamano], empresa3]
+        escenario.ix[escenario.index[0:tamano - tarea], 3 * tarea+3] = df.ix[df.index[tarea:tamano], 'NOMBRE_ESV']
+        return escenario
